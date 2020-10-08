38a3e1c6c0deccbfddc97f5d14d4a0586a2832d2
Zac Sweers <pandanomic@gmail.com>
2020-10-08 17:14:47 -0400

    Spotless

--- src/test/kotlin/com/slack/retrofit/ResultTypeTest.kt
+++ src/test/kotlin/com/slack/retrofit/ResultTypeTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 Slack Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.slack.retrofit
 
 import com.google.common.truth.Truth.assertThat
@@ -50,7 +65,6 @@ class ResultTypeTest {
   private fun Type.assertEqualTo(other: Type) {
     assertThat(Types.equals(this, other)).isTrue()
   }
-
 
   private inline fun <reified T> testType() {
     testType(typeOf<T>())
