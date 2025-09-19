eea80b9183351143bab1195d7d59e9d66f95bdbb
Zac Sweers <pandanomic@gmail.com>
2025-09-19 16:11:06 +0900

    Is it the names?

--- integrations/ktor/src/commonMain/kotlin/com/slack/eithernet/integration/ktor/ktorEitherNetExtensions.kt
+++ integrations/ktor/src/commonMain/kotlin/com/slack/eithernet/integration/ktor/ktorEitherNetExtensions.kt
@@ -53,13 +53,13 @@ internal fun <E : Any> Exception.asKtorApiResult(): ApiResult<Nothing, E> {
       ApiResult.httpFailure(response.status.value)
     }
     is ConnectTimeoutException -> {
-      ApiResult.networkFailure(IOException(this))
+      ApiResult.networkFailure(IOException("", this))
     }
     is SocketTimeoutException -> {
-      ApiResult.networkFailure(IOException(this))
+      ApiResult.networkFailure(IOException("", this))
     }
     is UnresolvedAddressException -> {
-      ApiResult.networkFailure(IOException(this))
+      ApiResult.networkFailure(IOException("", this))
     }
     else -> {
       ApiResult.unknownFailure(this)
